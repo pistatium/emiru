21c46945b5ede042f6c90571458de5adb1ff92c8
kimihiro_n <394378+pistatium@users.noreply.github.com>
2021-01-23 19:53:34 +0900

    Init repo

---
+++ server/go.mod
@@ -0,0 +1,8 @@
+module github.com/pistatium/emiru
+
+go 1.13
+
+require (
+	github.com/dghubble/go-twitter v0.0.0-20201011215211-4b180d0cc78d
+	github.com/dghubble/oauth1 v0.7.0
+)
---
+++ server/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+
+	"github.com/dghubble/go-twitter/twitter"
+	"github.com/dghubble/oauth1"
+)
+
+func main() {
+	config := oauth1.NewConfig("consumerKey", "consumerSecret")
+	token := oauth1.NewToken("accessToken", "accessSecret")
+	httpClient := config.Client(oauth1.NoContext, token)
+	client := twitter.NewClient(httpClient)
+}
