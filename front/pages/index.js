111c8e076c591eac5e89a7b21c8205fc6631b9e0
kimihiro_n <394378+pistatium@users.noreply.github.com>
2021-01-27 22:31:42 +0900

    WIP layout

--- front/pages/index.js
+++ front/pages/index.js
@@ -9,17 +9,24 @@ export default function Home() {
                 url={''}
             />
 
-            <div className="relative bg-white overflow-hidden">
-                <h1>emiru</h1>
+            <div className="container mx-auto flex flex-col md:flex-row items-center my-12 md:my-24">
+                <div className="flex flex-col w-full lg:w-1/2 justify-center items-start pt-12 pb-24 px-6">
+                    <p className="text-5xl tracking-loose">emiru</p>
+                    <h1 className="my-4">絵師のためのTwitterビューアー</h1>
+                    <p className="leading-normal mb-4">てすと</p>
+                    <form action="/app/login" method="GET">
+                        <button
+                            className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-400 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+                            Sign in with Twitter
+                        </button>
+                    </form>
+                </div>
+
+                <div className="w-full lg:w-1/2 lg:py-6 text-center">
+                    ここにろご
+                </div>
             </div>
 
-            <form className="max-w-md w-full" action="/app/login" method="GET">
-                <div>
-                    <button type="submit" className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-400 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
-                        Sign in with Twitter
-                    </button>
-                </div>
-            </form>
             <footer className="footer">
                 <a
                     href="https://github.com/pistatium"
