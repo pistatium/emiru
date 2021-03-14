71022c2f156acd73b10b750501774038f0f7c7d3
kimihiro_n <394378+pistatium@users.noreply.github.com>
2021-03-14 22:15:27 +0900

    Set pwa

--- front/components/header.tsx
+++ front/components/header.tsx
@@ -16,31 +16,42 @@ const Header: React.FC<Props> = ({ children, title, image, url, menu, descriptio
         <>
             <Head>
                 <title>{title}</title>
+                <link rel="canonical" href={url} />
+
                 <meta name="viewport" content="width=device-width,initial-scale=1" />
+                <meta name="application-name" content="emiru(β)" />
+                <meta name="theme-color" content="#40b9ff" />
+                <meta name="description" content={description} />
+
+                <meta name="apple-mobile-web-app-capable" content="yes" />
+                <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+                <meta name="apple-mobile-web-app-title" content="emiru(β)" />
+                <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon-180x180.png" />
+
                 <meta property="og:title" content={title} />
                 <meta property="og:type" content="website" />
                 <meta property="og:description" content={description} />
                 <meta property="og:url" content={url} />
                 <meta property="og:image" content={`${url}{image}`} />
-                <meta property="og:site_name" content={title} />
+                <meta property="og:site_name" content="emiru(β)" />
+                <link rel="icon" type="image/vnd.microsoft.icon" href="/images/favicon.ico" />
+                <link rel="icon" type="image/png" sizes="512x512" href="/images/android-chrome-512x512.png" />
+                <link rel="icon" type="image/png" sizes="128x128" href="/images/icon-128x128.png" />
+                <link rel="icon" type="image/png" sizes="512x512" href="/images/icon-512x512.png" />
+
+                <link rel="manifest" href="/images/manifest.json" />
+
                 <meta name="twitter:card" content="summary" />
                 <meta name="twitter:url" content={image} />
                 <meta name="twitter:title" content={title} />
                 <meta name="twitter:image" content={`${url}{image}`} />
                 <meta name="twitter:description" content={description} />
-                <link rel="canonical" href={url} />
+
                 <meta name="msapplication-square70x70logo" content="/site-tile-70x70.png" />
                 <meta name="msapplication-square150x150logo" content="/site-tile-150x150.png" />
                 <meta name="msapplication-wide310x150logo" content="/site-tile-310x150.png" />
                 <meta name="msapplication-square310x310logo" content="/site-tile-310x310.png" />
                 <meta name="msapplication-TileColor" content="#0078d7" />
-                <link rel="shortcut icon" type="image/vnd.microsoft.icon" href="/images/favicon.ico" />
-                <link rel="icon" type="image/vnd.microsoft.icon" href="/images/favicon.ico" />
-                <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon-180x180.png" />
-                <link rel="icon" type="image/png" sizes="512x512" href="/images/android-chrome-512x512.png" />
-                <link rel="icon" type="image/png" sizes="128x128" href="/images/icon-128x128.png" />
-                <link rel="icon" type="image/png" sizes="512x512" href="/images/icon-512x512.png" />
-                <link rel="manifest" href="/images/manifest.json" />
             </Head>
             <div className="flex items-center justify-between bg-gray-800 fixed w-full h-12 z-50 px-4">
                 <img src="/images/emiru_small.png" className="h-10" />
