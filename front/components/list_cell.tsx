8c0924b431c1387b935306413d18e343346b6165
kimihiro_n <394378+pistatium@users.noreply.github.com>
2021-03-23 21:14:54 +0900

    WIP list

--- front/components/list_cell.tsx
+++ front/components/list_cell.tsx
@@ -7,12 +7,22 @@ interface Props {
 
 const ListCell: React.FC<Props> = ({ list }) => {
     return (
-        <div className="w-full bg-white p-4">
-            <div>{list.name}</div>
-            <div>{list.description}</div>
-            <div className="flex">
-                <img src={list.creator.icon} className="h-4 w-4 mr-1" />
-                <div className="text-sm">{list.creator.name}</div>
+        <div className="w-full bg-white p-4 flex">
+            <label className="flex justify-start items-start mr-2">
+                <div className="bg-white border-2 rounded border-gray-400 w-6 h-6 flex flex-shrink-0 justify-center items-center mr-2 focus-within:border-blue-500">
+                    <input type="checkbox" className="opacity-0 absolute" />
+                    <svg className="fill-current hidden w-4 h-4 text-green-500 pointer-events-none" viewBox="0 0 20 20">
+                        <path d="M0 11l2-2 5 5L18 3l2 2L7 18z" />
+                    </svg>
+                </div>
+            </label>
+            <div className="flex-1">
+                <div>{list.name}</div>
+                <div>{list.description}</div>
+                <div className="flex">
+                    <img src={list.creator.icon} className="h-4 w-4 mr-1" />
+                    <div className="text-sm">{list.creator.name}</div>
+                </div>
             </div>
         </div>
     )
--- front/pages/edit_lists.tsx
+++ front/pages/edit_lists.tsx
@@ -9,14 +9,19 @@ import ListCell from '../components/list_cell'
 const fetcher = url => axios.get<GetListResponse>('/app/api/lists').then(res => res.data)
 
 export default function EditList(props) {
-    const { data, error } = useSWR('lists', fetcher)
+    // const { data, error } = useSWR('lists', fetcher)
+    const data = { lists: [] }
     return (
         <div className="bg-gray-100">
             <Header title={'emiru - リスト編集'} image={''} url={''} />
-            <div className="lg:container xl:mx-auto xl:px-24">
-                {data?.lists.map(l => (
-                    <ListCell list={l} />
-                ))}
+
+            <div className="lg:container xl:mx-auto xl:px-24 divide-y">
+                <div className="bg-white">
+                    <h2>リスト編集</h2>
+                    {data?.lists.map(l => (
+                        <ListCell list={l} />
+                    ))}
+                </div>
             </div>
             <Footer></Footer>
         </div>
