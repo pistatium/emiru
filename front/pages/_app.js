746489a1e5643774a9696fd21e53fc0f6557c68a
kimihiro_n <394378+pistatium@users.noreply.github.com>
2021-03-29 21:53:56 +0900

    Add ga

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -1,7 +1,9 @@
 import '../styles/globals.css'
+import Router from 'next/router'
+import withGA from 'next-ga'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return <Component {...pageProps} />
 }
 
-export default MyApp
+export default withGA('UA-193136339-1', Router)(MyApp)
