554c0b7d78936f431a779f2eaff718b1eb5bcaf3
kimihiro_n <394378+pistatium@users.noreply.github.com>
2021-04-06 21:38:57 +0900

    Fixed error on ads

--- front/components/ad.tsx
+++ front/components/ad.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
 import AdSense from 'react-adsense'
+import { ErrorBoundary } from 'react-error-boundary'
+
+function ErrorFallback({ error, resetErrorBoundary }) {
+    return <div className="bg-white rounded-md" />
+}
 
 const Ad: React.FC = () => {
-    return <AdSense.Google client="ca-pub-2359565431337443" slot="8532085154" style={{ display: 'block' }} layout="in-article" format="fluid" />
+    return (
+        <ErrorBoundary FallbackComponent={ErrorFallback}>
+            <AdSense.Google client="ca-pub-2359565431337443" slot="8532085154" style={{ display: 'block' }} layout="in-article" format="fluid" />
+        </ErrorBoundary>
+    )
 }
 
 export default Ad
