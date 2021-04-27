57ab25b0070ebef429565a455971a8dcde5e379c
kimihiro_n <394378+pistatium@users.noreply.github.com>
2021-04-27 21:32:20 +0900

    Add TWA

---
+++ twa/app/src/main/java/dev/pistatium/emiru/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package dev.pistatium.emiru.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
